2834dc1136ec56b2901eeb9cc20a38e3cb0d3bdf
MoreCircless <manuabril2001@outlook.es>
2024-05-12 18:06:10 +0200

    0.5

--- 03_TreasureIsland.py
+++ 03_TreasureIsland.py
@@ -2,27 +2,53 @@
 
 print("Welcome to Treasure Island!!!\nYour mission is to find the treasure")
 
+def treasure():
+    print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+    
+gameover = 0
+print('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n')
 
-print("step 1")
-movement1 = input("What you do? (L)eft or (R)ight?")
-
-if movement1 != "l" or movement1 != "L":
-    print("You fall into a hole noooo...\nGAME OVER")
-elif movement1 == "l" or movement1 == "L":
-    print("step 2")
-    movement2 = input("What do you choose? (S)wim or (W)ait?")
-    if movement2 != "W" or movement2 != "w":
-        print("OH NO!!, you have being attacked by trouts\nGAME OVER")
-    if  movement2 == "W" or movement2 == "w":
-        print("step 3")
-        movement3 = input("Which door do you choose? (R)ed, (Y)ellow or (B)lue?")
-        if movement3 == "R" or movement3 == "r":
-            print("YOU HAVE BEEN BURNED!!!\nGAME OVER")
-        elif movement3 == "Y" or movement3 == "y":
+step1 = input("Do you go (L)eft or (R)ight?\n-> ")
+if step1 == "L":
+    print("You\'ve come to a lake. There is an island in the middle of the lake.")
+    step2 = input("(S)wim or (W)ait?\n-> ")
+    if step2 == "W":
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.")
+        step3 = input("Which door? (R)ed, (Y)ellow or (B)lue\n-> ")
+        if step3 == "R":
+            print("Burned by fire!!!")
+        elif step3 == "Y":
+            treasure()
             print("YOU WIN!!!")
-        elif movement3 == "B" or movement3 == "b":
-            print("YOU ARE BEING EATEN BY BEAST!!\nGAME OVER")
-        else:
-            print("Game over")
+            gameover += 1
+        elif step3 == "B":
+            print("You are being eaten by beasts!!")
+    else:
+        print("You are being attacked by trout!!")
+else:
+    print("You fall into a hole!!!")
 
-print("Game finished!")
+if gameover == 0:
+    print("GAME OVER")
+
