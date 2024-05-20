24786ee511d31638d9b2f6f63e360b46634adbdc
Manu <manuabril2001@outlook.es>
2024-05-20 19:40:25 +0200

    coffe

--- 14_HigherLower/higherlower.py
+++ 14_HigherLower/higherlower.py
@@ -3,20 +3,31 @@ from art import logo
 from art import vs
 from data import data
 import random
+
+
 print(logo)
 
 
-data_lenght = len(data)
-first_person = data[random.randint(0, data_lenght-1)]
-
-print(first_person)
-print(first_person)
-print(len(data))
-#print(data)
-def vs():
-    print(vs)
+print(vs)
 
 
-name = first_person[name]
+first_option = random.choice(data)
 
-print(first_person[name])
+
+name = first_option["name"]
+followers = first_option["follower_count"]
+description = first_option["description"]
+country = first_option["country"]
+first_person_data = [name, followers, description, country]
+
+print(f"{first_person_data[0]} is a {first_person_data[2]} from {first_person_data[3]}, it has {first_person_data[1]} millions of followers")
+
+second_option = random.choice(data)
+
+while second_option == first_option:
+    second_option = random.choice(data)
+
+print(second_option)
+
+
+def data_extract():
---
+++ CoffeMachine/coffemachine.py
@@ -0,0 +1,121 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+            "milk": 0,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+logo = """
+   ( (
+    ) )
+  ........
+  |      |]
+  \      /    
+   `----'
+"""
+
+money = 0
+
+def ing_check(ing) -> bool:
+   res_needed = MENU[ing]
+   ret = True 
+   ingre = res_needed["ingredients"]
+   if resources["water"] < ingre["water"]:
+      ret = False
+   if resources["milk"] < ingre["milk"]:
+      ret = False 
+   if resources["coffee"] < ingre["coffee"]:
+      ret = False 
+   return ret 
+
+def no_resources():
+   print("There is no resources enough!\n Sorry")
+
+def payment(choice):
+   global money
+   quarters = input("Quarters -> ")
+   dimes = input("Dimes -> ")
+   nickles = input("Nickles -> ")
+   pennies = input("Pennies -> ")
+   coffe_choice = MENU[choice]
+   total_payment = 0.25 * float(quarters) + 0.10 * float(dimes) + 0.05 * float(nickles) + 0.01 * float(pennies)
+   print(f"Your payment: {total_payment: .2f}$")
+   if float(coffe_choice["cost"]) >= total_payment:
+      print("Not enough money!\nReturning money...")
+      return False     
+   else:
+      print("Enough money!")
+      money += coffe_choice["cost"]
+      return_money = total_payment - coffe_choice["cost"]
+      print(f"Your return {return_money: .2f}$")
+      return True 
+
+   
+def product_update()
+
+
+def report():
+   print(f"\nWater -> {resources['water']}ml\nMilk -> {resources['milk']}ml\nCoffee -> {resources['coffee']}gr\nMoney -> {money}$")
+
+print(logo)
+print("Welcome to the coffe machine!")
+
+while True:
+   print("What would you like? (espresso/latte/cappuccino)")
+
+   user_choice = input("-> ")
+   if user_choice == "espresso":
+      if ing_check("espresso"):
+         payment_check = payment("espresso")
+         if payment_check == True:
+            product_update("espresso")
+      else:
+         no_resources()
+   elif user_choice == "latte":
+      if ing_check("latte"):
+         payment_check = payment("latte")
+         if payment_check == True:
+            product_update("latte")
+      else:
+         no_resources()
+   elif user_choice == "cappuccino":
+      if ing_check("cappuccino"):
+         payment_check = payment("cappuccino")
+         if payment_check == True:
+            product_update("espresso")
+      else: 
+         no_resources()
+   elif user_choice == "off":
+      break
+   elif user_choice == "report":
+      report()
+   else:
+      print("error!, not valid input")
+
