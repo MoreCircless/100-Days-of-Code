44536ac9eeeafbabd827791524dbc343fbcc6a7f
Manu <manuabril2001@outlook.es>
2024-05-20 11:58:21 +0200

    0.1

---
+++ 14_HigherLower/higherlower.py
@@ -0,0 +1,22 @@
+from os import name
+from art import logo 
+from art import vs
+from data import data
+import random
+print(logo)
+
+
+data_lenght = len(data)
+first_person = data[random.randint(0, data_lenght-1)]
+
+print(first_person)
+print(first_person)
+print(len(data))
+#print(data)
+def vs():
+    print(vs)
+
+
+name = first_person[name]
+
+print(first_person[name])
