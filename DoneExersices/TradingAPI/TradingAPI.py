2b73a337de5fb47c8c04bb3ed1528d67320f8708
Manu <manuabril2001@outlook.es>
2024-06-09 10:26:24 +0200

    DAY 38

--- DoneExersices/TradingAPI/TradingAPI.py
+++ DoneExersices/TradingAPI/TradingAPI.py
@@ -16,20 +16,15 @@ NEWS_KEY = "135f55c35ac3483093db8b835cec0011"
 stock_rq = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&interval=5min&apikey={STOCK_KEY}")
 stock_ft = stock_rq.json()
 
-last_refresh = stock_ft["Meta Data"]["3. Last Refreshed"] # Date of the last refresh
+last_refresh = stock_ft["Meta Data"]["3. Last Refreshed"] 
 one_day = timedelta(days=1)
 close_value = stock_ft["Time Series (Daily)"][last_refresh]["4. close"]
 last_refresh_ft = datetime.strptime(last_refresh, "%Y-%m-%d")
 bf_close_value = stock_ft["Time Series (Daily)"][f"{last_refresh_ft.date() - timedelta(days=1)}"]["4. close"]
 
 
-close_value = 177.4800
-bf_close_value = 190
-last_refresh = "2024-6-6"
-
-
-change = abs(close_value - bf_close_value)
-change_ptg = (change/bf_close_value * 100)
+change = abs(float(close_value) - float(bf_close_value))
+change_ptg = (float(change)/float(bf_close_value) * 100)
 
 
 if close_value > bf_close_value: 
@@ -40,10 +35,10 @@ else:
 news_call = requests.get(f"https://newsapi.org/v2/everything?q=tesla&apiKey={NEWS_KEY}")
 news_call = news_call.json()
 
-if change_ptg >= 5:
-    print("\n\nNEWS!\n")
-    for n in range(3):
-        print(f"{news_call["articles"][n]["title"]}\n{news_call["articles"][n]["description"]}\nSource: {news_call["articles"][0]["url"]}\n")
+
+print("\n\nNEWS!\n")
+for n in range(3):
+    print(f"{news_call["articles"][n]["title"]}\n{news_call["articles"][n]["description"]}\nSource: {news_call["articles"][0]["url"]}\n")
 
 
 
