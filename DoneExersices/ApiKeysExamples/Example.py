be13b62c4abf764fe1cc52dae9184261e69f0320
Manu <manuabril2001@outlook.es>
2024-06-14 13:20:26 +0200

    api

--- DoneExersices/ApiKeysExamples/Example.py
+++ DoneExersices/ApiKeysExamples/Example.py
@@ -9,10 +9,31 @@ KEY = "3afd900ff7562bec5125ffc62dd69a57"
 MY_LAT = 42.015350
 MY_LON = -4.540560
 
-ex_rq = f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LAT}&lon={MY_LON}&appid={KEY}"
+OWM_ENDPOINT = f"https://api.openweathermap.org/data/2.5/forecast"
+
+wheathet_params = {
+    "lat": MY_LAT,
+    "lon": MY_LON,
+    "appid": KEY,
+    "cnt": 4,
+}
 
 
-data = requests.get(url=ex_rq)
+data = requests.get(url= OWM_ENDPOINT, params=wheathet_params)
 f_data = data.json()
-print(f_data)
 
+# status = f_data["list"][0]["weather"]
+# print(status)
+
+rain = False
+
+for hour_data in f_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        rain = True
+        
+
+if rain:
+    print("GET A UMBRELLA!")
+else:
+    print("NO UMBRELLA NECESARY")
